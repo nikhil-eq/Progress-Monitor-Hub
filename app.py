5a59a2115b134437bbaf8bf62f5bee7e3ebbee57
nikhil-eq <nikhil@equilibriumearth.com>
2026-09-03 17:26:09 +0530

    fix: logo size | title change

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Progress Monitor Hub", layout="wide")
+st.set_page_config(page_title=" EQ <> GC Progress Monitor Hub", layout="wide")
 
 st.markdown("""
     <style>
@@ -26,7 +26,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.logo("https://github.com/nikhil-eq/comprehensive-project-management/blob/main/eq%20-%20white.png?raw=true", size = 'large', )
+st.logo("https://github.com/nikhil-eq/comprehensive-project-management/blob/main/eq%20-%20white.png?raw=true", size = 'medium', )
 st.title('Progress Monitor Hub')
 
 pg = st.navigation([st.Page("daily_entry.py", title = 'Daily Log Entry'), 
